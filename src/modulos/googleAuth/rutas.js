a7c436a933ecd3942a827b8d8fc0358fe431af80
Al Rubio <alrubiom.web@gmail.com>
2024-05-09 00:43:45 +0200

    Configuración cookies

--- src/modulos/googleAuth/rutas.js
+++ src/modulos/googleAuth/rutas.js
@@ -60,13 +60,13 @@ router.get('/callback', async (req, res, next) => {
 // Enviar el nombre del usuario y el rol en cookies accesibles desde el frontend
 		res.cookie('userName', userInfo.name, {
 			secure: false, // Asegura que la cookie se envíe solo a través de HTTPS
-			sameSite: 'None' // Permite que la cookie sea enviada en requests de tipo top-level navigation que provengan de otros
+			sameSite: 'strict' // Permite que la cookie sea enviada en requests de tipo top-level navigation que provengan de otros
 			// sitios
 		});
 
 		res.cookie('userRol', rol, {
 			secure: false,
-			sameSite: 'None'
+			sameSite: 'strict'
 		});
 
 		res.redirect('https://www.bakurentalmanager.site');
