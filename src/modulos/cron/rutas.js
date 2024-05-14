e3f9fa209d6d43504b3574390c7b4d4263ea76a8
Al Rubio <alrubiom.web@gmail.com>
2024-05-14 23:10:21 +0200

    Añadir archivo de rutas cron para llamar a la función de revisión y agregación de movimientos periódicos automáticamente

---
+++ src/modulos/cron/rutas.js
@@ -0,0 +1,39 @@
+/**
+ * Baku - Sistema Integral de Gestión Inmobiliaria
+ *
+ * Este archivo es parte de "Baku - Sistema Integral de Gestión Inmobiliaria".
+ *
+ * Este código está disponible bajo la Licencia Dual:
+ * - Uso no comercial bajo GNU AGPL v3.0.
+ * - Uso comercial bajo Licencia Comercial (contactar al autor para más detalles).
+ *
+ * @author Alfredo Rubio Mariño
+ * @copyright (C) 2023 Alfredo Rubio Mariño
+ * @license AGPL-3.0-or-later <https://www.gnu.org/licenses/agpl-3.0.html>
+ */
+
+	// Importación de módulos necesarios
+const express = require('express');
+const respuesta = require('../../red/respuestas'); // Módulo para manejar respuestas
+const controlador = require('./index'); // Importa el controlador de contactos
+const seguridadRol = require ('../usuarios/seguridadRol'); // Importa el middleware para comprobar rol de usuario
+
+// Creación del router de Express
+const router = express.Router();
+
+// Definición de rutas.js
+router.get('/', seguridadRol(), revisarYGenerarMovimientos);
+
+// Función para llamar a la revisión de movimientos periódicos y generar automáticamente si procede
+async function revisarYGenerarMovimientos(req, res, next) {
+	try {
+		await controlador.revisarYGenerarMovimientos();
+		respuesta.success(req, res, 'Movimientos revisados/generados correctamente', 200);
+	} catch (err) {
+		next(err);
+	}
+};
+
+
+// Exporta el router para ser usado en otros archivos
+module.exports = router;
